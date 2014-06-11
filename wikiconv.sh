2f2be5820fd647b1502cfd11afc05272da718e99
ayuthky <ryuayu.rose@gmail.com>
2014-06-11 11:56:54 +0900

    こめんとをきれいに

--- wikiconv.sh
+++ wikiconv.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # euccnv2utf8
-# pukiwiki files 
+# pukiwiki files conv
 for f in `ls *.txt`
 do
+	# convert file encode output tmp.txt
     iconv -f EUC -t UTF8 < $f > tmp.txt
+	# rename dist filename
 	mv tmp.txt $f
 done
 
